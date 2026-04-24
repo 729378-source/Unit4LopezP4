c014a1f53b3377ed0fb00e91204dfda991307dac
729378-PL <729378@student.dusd.net>
2026-04-24 11:46:38 -0700

    doner
    doner

--- P4LopezUnit4/Assets/Scripts/PlayerController.cs
+++ P4LopezUnit4/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,11 @@ public class PlayerController : MonoBehaviour
 {
 
     private Rigidbody playerRb;
-    public float speed = 5.0f;
     private GameObject focalPoint;
+    private float powerUpStrength = 15.0f;
+    public float speed = 5.0f;
+    public bool hasPowerup = false;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,6 +21,27 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+
+        playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<enemyRigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+
+            enemyRigidbody.AddForce(awayFromPlayer * 10, ForceMode.Impulse);
+            Debug.Log("Collided with: " + collision.gameObject.name + " with powerup set to " + hasPowerup);
+        }
     }
 }
